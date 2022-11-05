b2577757c38416be9a12680e968b09f5e015c17f
DIEHARD0007 <52812834+DIEHARD0007@users.noreply.github.com>
2022-11-05 18:26:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -130,7 +130,7 @@ function App() {
     setFeedback(`Minting your ${CONFIG.NFT_NAME}...`);
     setClaimingNft(true);
     blockchain.smartContract.methods
-      .discovermytoken(blockchain.account, mintAmount)
+      .discovermytoken(1)
       .send({
         gasLimit: String(totalGasLimit),
         to: CONFIG.CONTRACT_ADDRESS,
