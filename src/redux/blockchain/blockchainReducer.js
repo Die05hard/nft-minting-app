b655f4c1acf4be8f799a40419264b20a505c5bd1
Die05hard <115734003+Die05hard@users.noreply.github.com>
2022-11-07 23:35:35 +0530

    Update blockchainReducer.js

--- src/redux/blockchain/blockchainReducer.js
+++ src/redux/blockchain/blockchainReducer.js
@@ -19,6 +19,7 @@ const blockchainReducer = (state = initialState, action) => {
         loading: false,
         account: action.payload.account,
         smartContract: action.payload.smartContract,
+        RedeemSmartContract: action.payload.RedeemSmartContract,
         web3: action.payload.web3,
       };
     case "CONNECTION_FAILED":
